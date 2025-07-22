fbc0e218a21f218ea8e046eca0435e08fecf9357
gglluukk <gglluukk@users.noreply.github.com>
2025-07-23 00:44:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,22 +2,18 @@
 # V2ray for Alwaysdata.com
 
 TMP_DIRECTORY=$(mktemp -d)
-
-UUID=$(grep -o 'UUID=[^ ]*' $HOME/admin/config/apache/sites.conf | sed 's/UUID=//')
-VMESS_WSPATH=$(grep -o 'VMESS_WSPATH=[^ ]*' $HOME/admin/config/apache/sites.conf | sed 's/VMESS_WSPATH=//')
-VLESS_WSPATH=$(grep -o 'VLESS_WSPATH=[^ ]*' $HOME/admin/config/apache/sites.conf | sed 's/VLESS_WSPATH=//')
-
 UUID=${UUID:-`uuidgen`}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 URL=${USER}.alwaysdata.net
 
-wget -q -O $TMP_DIRECTORY/config.json https://raw.githubusercontent.com/hiifeng/V2ray-for-AlwaysData/main/config.json
+wget -q -O $TMP_DIRECTORY/config.json https://raw.githubusercontent.com/gglluukk/V2ray-for-AlwaysData/main/config.json
 wget -q -O $TMP_DIRECTORY/v2ray-linux-64.zip https://github.com/v2fly/v2ray-core/releases/download/v5.37.0/v2ray-linux-64.zip
+rm -f v2ray
 unzip -oq -d $HOME $TMP_DIRECTORY/v2ray-linux-64.zip v2ray
 
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g;s#10000#8300#g;s#20000#8400#g;s#127.0.0.1#0.0.0.0#g" $TMP_DIRECTORY/config.json
-cp $TMP_DIRECTORY/config.json $HOME
+/bin/cp -f $TMP_DIRECTORY/config.json $HOME
 rm -rf $HOME/admin/tmp/*.*
 
 Advanced_Settings=$(cat <<-EOF
